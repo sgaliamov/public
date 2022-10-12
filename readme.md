cc9127f73b17321e0c5ea5b6c94fb5e1132a4561
s. galiamov <salavat.galiamov@transavia.com>
2022-10-12 16:55:22 +0200

    inline list

--- readme.md
+++ readme.md
@@ -3,5 +3,5 @@
 ## Chocolatey packages
 
 ``` ps1
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/sgaliamov/public/master/install-choco-packages.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/sgaliamov/public/choco/scripts/install-choco-packages.ps1'))
 ```
--- scripts/install-choco-packages.ps1
+++ scripts/install-choco-packages.ps1
@@ -2,7 +2,41 @@ Clear-Host
 
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-$lines = Get-Content "$PSScriptRoot/choco-packages" | Where-Object { -not $_.StartsWith('#') }
+$lines = "
+nodejs-lts
+7-taskbar-tweaker
+7zip.install
+azure-cli
+ditto.install
+drawio
+evernote
+fiddler
+FiraCode
+foxitreader
+git.install
+git-fork
+ilspy
+kdiff3
+keepass.install
+linqpad
+megasync
+meld
+microsoft-windows-terminal
+neovim
+paint.net
+postman
+powershell-core
+powertoys
+servicebusexplorer
+sql-server-management-studio
+telegram.install
+tor-browser
+totalcommander
+vscode.install
+WhatsApp
+yarn
+# spotify
+" | Where-Object { -not $_.StartsWith('#') }
 
 foreach ($item in $lines) {
     choco install $item -y --ignorechecksum
